013d53387556902b3e663d48c5a654b6924f75a3
Roberto <robpalacios1@hotmail.com>
2018-11-22 22:54:58 -0500

    Crea la ruta DELETE /products/:id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,14 +30,22 @@ class ProductsController < ApplicationController
 
 
 	def update
-		@product = Product.find(params[:id])
-		if @product.update(product_params)
+		product = Product.find(params[:id])
+		if product.update(product_params)
 			redirect_to products_path, notice: "El producto ha sido modificado con Exito"
 		else
 			render :edit
 		end
 	end
+
 	
+	def destroy
+		@product = Product.find(params[:id])
+		product.destroy
+
+		redirect_to products_path, notice: "El producto fue eliminado con Exito"
+	end
+
 
 	private
 		def product_params
