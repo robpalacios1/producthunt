4f65eb234a11927d97312f2cdcccabd2898b28ca
Roberto <robpalacios1@hotmail.com>
2018-11-26 23:01:41 -0500

    Implementa la funcionalidad de comentarios

---
+++ app/models/comment.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  product_id :integer
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Comment < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+
+  belongs_to :product
+  belongs_to :user
+end
